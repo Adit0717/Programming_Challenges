75bf448f59e3f4e0b927436f1614147a90256b89
Aditya Krishnamurthi <krisa04@pfw.edu>
2025-10-25 23:04:26 -0400

    Comp 473 #2

--- Contests/Weekly/#473.cpp
+++ Contests/Weekly/#473.cpp
@@ -20,3 +20,36 @@ public:
         return result;
     }
 };
+
+/*
+Q2. Maximum Alternating Sum of Squares
+You are given an integer array nums. You may rearrange the elements in any order.
+
+The alternating score of an array arr is defined as:
+
+score = arr[0]2 - arr[1]2 + arr[2]2 - arr[3]2 + ...
+Return an integer denoting the maximum possible alternating score of nums after rearranging its elements.
+*/
+
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        long long firstHalf = 0, secondHalf = 0;
+        
+        for(int &x : nums) {
+            x = x * x;
+        }
+        
+        sort(nums.begin(), nums.end());        
+        
+        for(int i = 0; i < (nums.size() / 2); i++) {
+            firstHalf += nums[i];
+        }
+
+        for(int i = nums.size() / 2; i < nums.size(); i++) {
+            secondHalf += nums[i];
+        }
+        
+        return secondHalf - firstHalf;
+    }
+};
