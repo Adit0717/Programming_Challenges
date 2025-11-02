19a4ab456e977666e85948c6664cdf6ea023f196
Aditya Krishnamurthi <krisa04@pfw.edu>
2025-11-02 00:22:07 -0400

    Weekly 474 #2

--- Contests/Weekly/#474.cpp
+++ Contests/Weekly/#474.cpp
@@ -35,3 +35,49 @@ public:
         return result;
     }
 };
+
+/*
+Maximum Product of Three Elements After One Replacement
+You are given an integer array nums.
+You must replace exactly one element in the array with any integer value in the range [-10^5, 10^5] (inclusive).
+After performing this single replacement, determine the maximum possible product of any three elements 
+at distinct indices from the modified array.
+
+Return an integer denoting the maximum product achievable.
+
+Input: nums = [-5,7,0]
+Output: 3500000
+Explanation:
+Replacing 0 with -10^5 gives the array [-5, 7, -10^5], 
+which has a product (-5) * 7 * (-10^5) = 3500000. 
+The maximum product is 3500000.
+*/
+
+class Solution {
+public:
+    long long maxProduct(vector<int>& nums) {
+        long long result = 0;
+        sort(nums.begin(), nums.end());
+
+        int l = 0, r = nums.size() - 1;
+        int temp = 2;
+        long long prod = 1;
+        
+        while(temp > 0) {
+            if(abs(nums[l]) >= abs(nums[r])) {
+                prod *= nums[l];
+                l++;
+            } else {
+                prod *= nums[r];
+                r--;
+            }
+            temp--;
+        }
+        result = prod;
+        cout << result;
+        if(prod < 0) result = result * pow(-10, 5);
+        else result = result * pow(10, 5);
+        
+        return result;
+    }
+};
